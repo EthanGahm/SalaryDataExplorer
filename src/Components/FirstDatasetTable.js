71404927bf36cdccb6ac13de8bc5f8d8da4e1222
EthanGahm <ethangahm@gmail.com>
2021-06-01 15:40:22 -0400

    Added async function in FirstDatasetTable to the TestTable export function and turned a couple of variables into hooks.

--- src/Components/FirstDatasetTable.js
+++ src/Components/FirstDatasetTable.js
@@ -1,48 +1,45 @@
-import * as React from "react";
+import React from "react";
 import { DataGrid } from "@material-ui/data-grid";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import SheetsAPIInfo from "../SheetsAPIInfo.json";
 import { GoogleSpreadsheet } from "google-spreadsheet";
 
-const columns = [
-  
-  { field: "Age", headerName: "Age", width: 130 },
-  { field: "Industry", headerName: "Industry", width: 130 },
-  { field: "Job Title", headerName: "Job Title", width: 130 },
-  { field: "Annual Salary", headerName: "Annual Salary", width: 130 },
-  { field: "Currency", headerName: "Currency", width: 130 },
-  { field: "Location", headerName: "Location", width: 130 },
-  { field: "Work Experience", headerName: "Work Experience", width: 130 },
-  { field: "Context for Job", headerName: "Context for Job", width: 130 },
-  { field: "Other", headerName: "Other", width: 130 },
-
-];
-
-let rows = [];
-
-const spreadSheet = new GoogleSpreadsheet(
-  "13vhiAvwNpCaUAAutNwd-pEbYZNk1qIVMqgB4e4HGQtE"
-);
-
-let isLoaded = false;
-
-(async function () {
-  try {
-    await spreadSheet.useServiceAccountAuth({
-      client_email: SheetsAPIInfo.client_email,
-      private_key: SheetsAPIInfo.private_key,
-    });
-
-    await spreadSheet.loadInfo();
-    const sheet = spreadSheet.sheetsByIndex[0];
-    rows = await sheet.getRows();
-    isLoaded = true;
-  } catch (e) {
-    console.error("Error: ", e);
-  }
-})();
-
 export default function TestTable() {
+  const [rows, setRows] = React.useState([]);
+  const [isLoaded, setIsLoaded] = React.useState(false);
+
+  const columns = [
+    { field: "Age", headerName: "Age", width: 130 },
+    { field: "Industry", headerName: "Industry", width: 130 },
+    { field: "Job Title", headerName: "Job Title", width: 130 },
+    { field: "Annual Salary", headerName: "Annual Salary", width: 130 },
+    { field: "Currency", headerName: "Currency", width: 130 },
+    { field: "Location", headerName: "Location", width: 130 },
+    { field: "Work Experience", headerName: "Work Experience", width: 130 },
+    { field: "Context for Job", headerName: "Context for Job", width: 130 },
+    { field: "Other", headerName: "Other", width: 130 },
+  ];
+
+  const spreadSheet = new GoogleSpreadsheet(
+    "13vhiAvwNpCaUAAutNwd-pEbYZNk1qIVMqgB4e4HGQtE"
+  );
+
+  (async function () {
+    try {
+      await spreadSheet.useServiceAccountAuth({
+        client_email: SheetsAPIInfo.client_email,
+        private_key: SheetsAPIInfo.private_key,
+      });
+
+      await spreadSheet.loadInfo();
+      const sheet = spreadSheet.sheetsByIndex[0];
+      setRows(await sheet.getRows());
+      setIsLoaded(true);
+    } catch (e) {
+      console.error("Error: ", e);
+    }
+  })();
+
   return (
     <div style={{ height: 600, width: "100%" }}>
       {isLoaded ? (
