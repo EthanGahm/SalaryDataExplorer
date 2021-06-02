725618584a33a0139c38a7302ae69af0ed8e5f29
EthanGahm <ethangahm@gmail.com>
2021-06-02 12:09:23 -0400

    Fixed bug where tables were not loading properly on page reload. Moved async function into component's export function, made local variables into state elements, and wrapped the async functions in useEffect() calls.

--- src/Components/FirstDatasetTable.js
+++ src/Components/FirstDatasetTable.js
@@ -5,7 +5,6 @@ import SheetsAPIInfo from "../SheetsAPIInfo.json";
 import { GoogleSpreadsheet } from "google-spreadsheet";
 
 const columns = [
-  
   { field: "Age", headerName: "Age", width: 130 },
   { field: "Industry", headerName: "Industry", width: 130 },
   { field: "Job Title", headerName: "Job Title", width: 130 },
@@ -15,34 +14,35 @@ const columns = [
   { field: "Work Experience", headerName: "Work Experience", width: 130 },
   { field: "Context for Job", headerName: "Context for Job", width: 130 },
   { field: "Other", headerName: "Other", width: 130 },
-
 ];
-
-let rows = [];
 
 const spreadSheet = new GoogleSpreadsheet(
   "13vhiAvwNpCaUAAutNwd-pEbYZNk1qIVMqgB4e4HGQtE"
 );
 
-let isLoaded = false;
-
-(async function () {
-  try {
-    await spreadSheet.useServiceAccountAuth({
-      client_email: SheetsAPIInfo.client_email,
-      private_key: SheetsAPIInfo.private_key,
-    });
-
-    await spreadSheet.loadInfo();
-    const sheet = spreadSheet.sheetsByIndex[0];
-    rows = await sheet.getRows();
-    isLoaded = true;
-  } catch (e) {
-    console.error("Error: ", e);
-  }
-})();
-
 export default function TestTable() {
+  const [isLoaded, setIsLoaded] = React.useState(false);
+  const [rows, setRows] = React.useState([]);
+
+  React.useEffect(() => {
+    (async function () {
+      try {
+        await spreadSheet.useServiceAccountAuth({
+          client_email: SheetsAPIInfo.client_email,
+          private_key: SheetsAPIInfo.private_key,
+        });
+
+        await spreadSheet.loadInfo();
+        const sheet = spreadSheet.sheetsByIndex[0];
+        let tempRows = await sheet.getRows();
+        setRows(tempRows);
+        setIsLoaded(true);
+      } catch (e) {
+        console.error("Error: ", e);
+      }
+    })();
+  }, []);
+
   return (
     <div style={{ height: 600, width: "100%" }}>
       {isLoaded ? (
