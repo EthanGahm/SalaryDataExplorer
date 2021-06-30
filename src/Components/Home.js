b863d7dc18135eef8cc6931a5321979808a42bac
JulietteLaburthe <jkl6et@virginia.edu>
2021-06-30 14:51:45 -0400

    added comments

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -20,16 +20,42 @@ import Title from "./Title";
 import PageTitle from "./PageTitle";
 import Copyright from "./Copyright";
 import useStyles from "./UseStyles.js";
-import { RadarChart, Radar, PolarGrid, PolarAngleAxis, PolarRadiusAxis, BarChart, Bar, PieChart, Pie, LineChart,
-   Line, CartesianGrid, XAxis, YAxis, Label, Tooltip, Legend} from 'recharts';
-
-const colors = ['#0088FE', '#00C49F', '#FFBB28']; 
-
-const data1 = [{"age": "under 18", "salary": 40}, {"age": "18-24", "salary": 70}, 
-{"age": "25-34", "salary": 130}, {"age": "35-44", "salary": 170}, {"age": "45-54", "salary": 220}, 
-{"age": "55-64", "salary": 250}, {"age": "65 or over", "salary": 300}];
-const data = [{"name": "Computing or Tech", "val": 100, "fill": colors[0]}, {"name": "Accounting, Banking & Finance", 
-"val": 50, "fill": colors[1]}, {"name": "Education (Higher Education)", "val": 170, "fill": colors[2]}];
+import {
+  RadarChart,
+  Radar,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  BarChart,
+  Bar,
+  PieChart,
+  Pie,
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Label,
+  Tooltip,
+  Legend,
+} from "recharts";
+// This is the home page, which holds a description for what features the app has.
+const colors = ["#0088FE", "#00C49F", "#FFBB28"];
+// data for intial pre-built graphs
+const data1 = [
+  { age: "under 18", salary: 40 },
+  { age: "18-24", salary: 70 },
+  { age: "25-34", salary: 130 },
+  { age: "35-44", salary: 170 },
+  { age: "45-54", salary: 220 },
+  { age: "55-64", salary: 250 },
+  { age: "65 or over", salary: 300 },
+];
+const data = [
+  { name: "Computing or Tech", val: 100, fill: colors[0] },
+  { name: "Accounting, Banking & Finance", val: 50, fill: colors[1] },
+  { name: "Education (Higher Education)", val: 170, fill: colors[2] },
+];
 
 export default function Home() {
   const classes = useStyles();
@@ -88,74 +114,130 @@ export default function Home() {
           <Grid container spacing={3}>
             <Grid item xs={12} md={8} lg={9}>
               <Paper className={classes.paper}>
+                {/* context about the app */}
                 <Title>About this Tool</Title>
-                This is a tool that allows users to access information about their salaries based on the
-                2021 ask a manager survey. Users could search for a specific job position or industry to
-                find salary data. We have charts and graphs to represent the different levels of salaries, 
-                and we could show corrolations between demographics and salary.
+                This is a tool that allows users to access information about
+                their salaries based on the 2021 ask a manager survey. Users
+                could search for a specific job position or industry to find
+                salary data. We have charts and graphs to represent the
+                different levels of salaries, and we could show corrolations
+                between demographics and salary.
               </Paper>
             </Grid>
 
-
-            <Grid item xs = {12} md = {8} lg = {9}>
-              <Paper className={classes.paper}>     
-                <center><h4>Salary Over Time for Computer Scientists</h4></center>                
-                <center><LineChart width={700} height={300} data={data1}
-                  margin={{ top: 20, right: 20, left: 20, bottom: 20 }}>
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="age" padding={{ left: 10, right: 10 }}>
-                    <Label value = "Age" offset={-15} position="insideBottom" />
-                  </XAxis>
-                  <YAxis label = {{value: "Salary", angle: -90, position: "insideLeft"}} />
-                  <Tooltip />
-                  <Line type="monotone" dataKey="salary" fill = {colors[0]} />
-                </LineChart></center>
+            <Grid item xs={12} md={8} lg={9}>
+              <Paper className={classes.paper}>
+                <center>
+                  <h4>Salary Over Time for Computer Scientists</h4>
+                </center>
+                <center>
+                  <LineChart
+                    width={700}
+                    height={300}
+                    data={data1}
+                    margin={{ top: 20, right: 20, left: 20, bottom: 20 }}
+                  >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="age" padding={{ left: 10, right: 10 }}>
+                      <Label value="Age" offset={-15} position="insideBottom" />
+                    </XAxis>
+                    <YAxis
+                      label={{
+                        value: "Salary",
+                        angle: -90,
+                        position: "insideLeft",
+                      }}
+                    />
+                    <Tooltip />
+                    <Line type="monotone" dataKey="salary" fill={colors[0]} />
+                  </LineChart>
+                </center>
               </Paper>
             </Grid>
           </Grid>
 
-
-          <Grid item xs = {12} md = {8} lg = {9}>
-            <Paper className = {classes.paper}>
-              <h4><center>Number of People for Each Degree</center></h4>
-              <center><PieChart width={300} height={350} margin = {{ top: 5, right: 0, left: 0, bottom: 5}}>
-                <Pie data = {data} dataKey = "val" nameKey = "name" cx = "50%" cy = "50%" outerRadius = {100} 
-                label />
-                <Legend />
-              </PieChart></center>
-              </Paper>
-          </Grid>
-
-
-          <Grid item xs = {12} md = {8} lg = {9}>
-              <Paper className = {classes.paper}>
-                <center><h4>Salaries for Different Jobs</h4></center>
-                <center><BarChart width = {700} height = {300} data = {data} 
-                margin={{ top: 20, right: 10, left: 10, bottom: 20 }}>
-                  <CartesianGrid strokeDasharray = "3 3" />
-                  <XAxis dataKey = "name" padding={{ left: 10, right: 10 }}/>
-                  <YAxis label = {{value: "Salary", angle: -90, position: "insideLeft"}}/>
-                  <Bar dataKey = "val" fill = {colors[2]} />
-                  <Tooltip />
-                </BarChart></center>
-              </Paper>
-          </Grid>
-
-
-          <Grid item xs = {12} md = {8} lg =  {9}>
-            <Paper className = {classes.paper}>
-              <center><h4>Distribution of People in the Job Market</h4></center>
-              <center><RadarChart outerRadius = {100} width = {600} height = {250} data = {data}>
-                <PolarGrid />
-                <PolarAngleAxis dataKey = "name"/>
-                <PolarRadiusAxis angle = {30} domain = {[0, 150]} />
-                <Radar name = "Men" dataKey = "val" fill = {colors[1]} fillOpacity = {0.5} animationBegin 
-                animationDuration = {3000} />
-                <Legend />
-              </RadarChart></center>
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper className={classes.paper}>
+              <h4>
+                <center>Number of People for Each Degree</center>
+              </h4>
+              <center>
+                <PieChart
+                  width={300}
+                  height={350}
+                  margin={{ top: 5, right: 0, left: 0, bottom: 5 }}
+                >
+                  <Pie
+                    data={data}
+                    dataKey="val"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={100}
+                    label
+                  />
+                  <Legend />
+                </PieChart>
+              </center>
             </Paper>
           </Grid>
 
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper className={classes.paper}>
+              <center>
+                <h4>Salaries for Different Jobs</h4>
+              </center>
+              <center>
+                <BarChart
+                  width={700}
+                  height={300}
+                  data={data}
+                  margin={{ top: 20, right: 10, left: 10, bottom: 20 }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="name" padding={{ left: 10, right: 10 }} />
+                  <YAxis
+                    label={{
+                      value: "Salary",
+                      angle: -90,
+                      position: "insideLeft",
+                    }}
+                  />
+                  <Bar dataKey="val" fill={colors[2]} />
+                  <Tooltip />
+                </BarChart>
+              </center>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper className={classes.paper}>
+              <center>
+                <h4>Distribution of People in the Job Market</h4>
+              </center>
+              <center>
+                <RadarChart
+                  outerRadius={100}
+                  width={600}
+                  height={250}
+                  data={data}
+                >
+                  <PolarGrid />
+                  <PolarAngleAxis dataKey="name" />
+                  <PolarRadiusAxis angle={30} domain={[0, 150]} />
+                  <Radar
+                    name="Men"
+                    dataKey="val"
+                    fill={colors[1]}
+                    fillOpacity={0.5}
+                    animationBegin
+                    animationDuration={3000}
+                  />
+                  <Legend />
+                </RadarChart>
+              </center>
+            </Paper>
+          </Grid>
 
           <Box pt={4}>
             <Copyright />
--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,7 +1,8 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Typography from '@material-ui/core/Typography';
+import * as React from "react";
+import PropTypes from "prop-types";
+import Typography from "@material-ui/core/Typography";
 
+// This component is used to title certain aspects of the application
 function Title(props) {
   return (
     <Typography component="h2" variant="h6" color="primary" gutterBottom>
