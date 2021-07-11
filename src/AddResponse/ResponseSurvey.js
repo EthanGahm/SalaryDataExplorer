9250ce1e9470fbe970811392d2b133c8e739cb42
Juliette Laburthe <jkl6et@virginia.edu>
2021-07-11 13:39:27 -0400

    removed not-working files

--- src/AddResponse/ResponseSurvey.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "survey-react/survey.css";
-import ResponseSurvey from "./Survey";
-
-const Survey = () => {
-  return <ResponseSurvey />;
-};
-
-export default Survey;
